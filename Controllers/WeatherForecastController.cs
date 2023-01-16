91c32166e0371e0b9f839b4446cdd51b5f40e650
Gabriel Teles <gabrieltbeserra@gmail.com>
2023-01-16 20:00:52 -0300

    First

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TesteMedeiros.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "GetWeatherForecast")]
+        public IActionResult Get([FromBody] List<int> ints)
+        {
+            var a = ints.OrderBy(x => x).ToArray();
+
+            return Ok(a);
+        }
+    }
+}
